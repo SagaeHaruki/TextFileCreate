f3f28aec74664726b39501970e2b4795ee063ad8
Hayase Yuuka <okeehnaruu@yahoo.com>
2023-11-16 21:40:50 +0900

    Add project files.

---
+++ RegisterForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace TextFileCreate
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.studNoTB = new System.Windows.Forms.TextBox();
+            this.fnameTB = new System.Windows.Forms.TextBox();
+            this.lnameTB = new System.Windows.Forms.TextBox();
+            this.miniTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ageTB = new System.Windows.Forms.TextBox();
+            this.contTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtPick = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.progrmTB = new System.Windows.Forms.ComboBox();
+            this.gendTB = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.regBTN = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.fileNameTB = new System.Windows.Forms.TextBox();
+            this.error = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student No:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age:";
+            // 
+            // studNoTB
+            // 
+            this.studNoTB.Location = new System.Drawing.Point(84, 49);
+            this.studNoTB.Name = "studNoTB";
+            this.studNoTB.Size = new System.Drawing.Size(106, 20);
+            this.studNoTB.TabIndex = 1;
+            // 
+            // fnameTB
+            // 
+            this.fnameTB.Location = new System.Drawing.Point(84, 81);
+            this.fnameTB.Name = "fnameTB";
+            this.fnameTB.Size = new System.Drawing.Size(106, 20);
+            this.fnameTB.TabIndex = 3;
+            // 
+            // lnameTB
+            // 
+            this.lnameTB.Location = new System.Drawing.Point(266, 81);
+            this.lnameTB.Name = "lnameTB";
+            this.lnameTB.Size = new System.Drawing.Size(106, 20);
+            this.lnameTB.TabIndex = 4;
+            // 
+            // miniTB
+            // 
+            this.miniTB.Location = new System.Drawing.Point(417, 81);
+            this.miniTB.Name = "miniTB";
+            this.miniTB.Size = new System.Drawing.Size(42, 20);
+            this.miniTB.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Last Name:";
+            // 
+            // ageTB
+            // 
+            this.ageTB.Location = new System.Drawing.Point(84, 111);
+            this.ageTB.Name = "ageTB";
+            this.ageTB.Size = new System.Drawing.Size(106, 20);
+            this.ageTB.TabIndex = 6;
+            // 
+            // contTB
+            // 
+            this.contTB.Location = new System.Drawing.Point(353, 140);
+            this.contTB.Name = "contTB";
+            this.contTB.Size = new System.Drawing.Size(106, 20);
+            this.contTB.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Date:";
+            // 
+            // dtPick
+            // 
+            this.dtPick.Location = new System.Drawing.Point(84, 140);
+            this.dtPick.Name = "dtPick";
+            this.dtPick.Size = new System.Drawing.Size(193, 20);
+            this.dtPick.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(211, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Program:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(215, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Gender:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(283, 143);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Contact No.:";
+            // 
+            // progrmTB
+            // 
+            this.progrmTB.FormattingEnabled = true;
+            this.progrmTB.Location = new System.Drawing.Point(266, 48);
+            this.progrmTB.Name = "progrmTB";
+            this.progrmTB.Size = new System.Drawing.Size(193, 21);
+            this.progrmTB.TabIndex = 2;
+            // 
+            // gendTB
+            // 
+            this.gendTB.FormattingEnabled = true;
+            this.gendTB.ItemHeight = 13;
+            this.gendTB.Location = new System.Drawing.Point(266, 110);
+            this.gendTB.Name = "gendTB";
+            this.gendTB.Size = new System.Drawing.Size(106, 21);
+            this.gendTB.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(383, 84);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "M.I.:";
+            // 
+            // regBTN
+            // 
+            this.regBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regBTN.Location = new System.Drawing.Point(375, 185);
+            this.regBTN.Name = "regBTN";
+            this.regBTN.Size = new System.Drawing.Size(84, 27);
+            this.regBTN.TabIndex = 11;
+            this.regBTN.Text = "Register";
+            this.regBTN.UseVisualStyleBackColor = true;
+            this.regBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(18, 193);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "File Name: ";
+            // 
+            // fileNameTB
+            // 
+            this.fileNameTB.Location = new System.Drawing.Point(84, 190);
+            this.fileNameTB.Name = "fileNameTB";
+            this.fileNameTB.Size = new System.Drawing.Size(106, 20);
+            this.fileNameTB.TabIndex = 10;
+            this.fileNameTB.TextChanged += new System.EventHandler(this.fileNameTB_TextChanged);
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.ForeColor = System.Drawing.Color.Red;
+            this.error.Location = new System.Drawing.Point(81, 174);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 13);
+            this.error.TabIndex = 19;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 224);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.fileNameTB);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.regBTN);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.gendTB);
+            this.Controls.Add(this.progrmTB);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtPick);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.contTB);
+            this.Controls.Add(this.ageTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.miniTB);
+            this.Controls.Add(this.lnameTB);
+            this.Controls.Add(this.fnameTB);
+            this.Controls.Add(this.studNoTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegisterForm";
+            this.Text = "Registration Form";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox studNoTB;
+        private System.Windows.Forms.TextBox fnameTB;
+        private System.Windows.Forms.TextBox lnameTB;
+        private System.Windows.Forms.TextBox miniTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ageTB;
+        private System.Windows.Forms.TextBox contTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtPick;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox progrmTB;
+        private System.Windows.Forms.ComboBox gendTB;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button regBTN;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox fileNameTB;
+        private System.Windows.Forms.Label error;
+    }
+}
+
---
+++ RegisterForm.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileCreate
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verifications First
+            try
+            {
+                // This section checks if all of the textbox have no valid input
+
+                if (string.IsNullOrEmpty(studNoTB.Text))
+                {
+                    error.Text = "Student Number cannot be empty";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (progrmTB.SelectedItem == null)
+                {
+                    error.Text = "Select your Program";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(lnameTB.Text))
+                {
+                    error.Text = "Enter your Last Name";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(fnameTB.Text))
+                {
+                    error.Text = "Enter your First Name";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(miniTB.Text))
+                {
+                    error.Text = "Enter your Middle Name";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(ageTB.Text))
+                {
+                    error.Text = "Enter your Age";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(miniTB.Text))
+                {
+                    error.Text = "Enter your Middle Name";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(ageTB.Text))
+                {
+                    error.Text = "Enter your Age";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (ageTB.Text.Any(char.IsLetter))
+                {
+                    error.Text = "Age Cannot contain letters";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (gendTB.SelectedItem == null)
+                {
+                    error.Text = "Select your Gender";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(contTB.Text))
+                {
+                    error.Text = "Enter your Contact Number";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                // Short Names
+                if (lnameTB.Text.Length > 15)
+                {
+                    error.Text = "Last Name is too long";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (fnameTB.Text.Length > 15)
+                {
+                    error.Text = "First Name is too long";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+                if (miniTB.Text.Length > 2)
+                {
+                    error.Text = "Middle Initial is too long";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+
+
+                // Symbols
+                if (Regex.IsMatch(lnameTB.Text, "[0-9]+$"))
+                {
+                    error.Text = "Last Name cannot contain any number or Symbol!";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+                if (Regex.IsMatch(fnameTB.Text, "[0-9]+$"))
+                {
+                    error.Text = "First Name cannot contain any number or Symbol!";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+                if (Regex.IsMatch(miniTB.Text, "[0-9]"))
+                {
+                    error.Text = "Middle Initial cannot contain any number or Symbol!";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+                if (Regex.IsMatch(contTB.Text, "[a-zA-Z\\W_]+$"))
+                {
+                    error.Text = "Contact Number cannot contain any letter or Symbol!";
+                    error.ForeColor = Color.Red;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+
+            // Path can be changed depends on the user preference
+            // You can find this on this file folder (TextFileCreate\bin\Debug) likely in the bin Debug File
+            string txtFilePath = "./FilesHere";
+
+            // Note you may also use this Example here
+            // string txtFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            string txtFileName = fileNameTB.Text + ".txt";
+            if (string.IsNullOrEmpty(txtFileName))
+            {
+                error.Text = "Enter a File name";
+                return;
+            }
+            string sNum = studNoTB.Text;
+            string fullName = fnameTB.Text + " " + miniTB.Text + " " + lnameTB.Text;
+            string program = progrmTB.Text;
+            string gender = gendTB.Text;
+            string ages = ageTB.Text;
+            string bday = dtPick.Value.ToString("yyyy-mm-dd");
+            string contact = contTB.Text;
+
+            string output = "Student No.: " + sNum +
+                "\nFullname: " + fullName + 
+                "\nProgram: " + program +
+                "\nGender: " + gender +
+                "\nAge: " + ages +
+                "\nBirthday: " + bday +
+                "\nContact Number: " + contact;
+
+            // When there is no longer an error this runs
+            error.Text = "Regisreted!";
+            error.ForeColor = Color.Green;
+            using (StreamWriter outputName = new StreamWriter(Path.Combine(txtFilePath, txtFileName)))
+            {
+                outputName.Write(output);
+            };
+
+            // Clears all the textbox and combo box
+            studNoTB.Text = "";
+            fnameTB.Text = "";
+            miniTB.Text= ""; 
+            lnameTB.Text = "";
+            progrmTB.SelectedIndex = -1;
+            gendTB.SelectedIndex = -1;
+            ageTB.Text = "";
+            contTB.Text = "";
+        }
+
+        private void fileNameTB_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileNameTB.Text))
+            {
+                error.Text = "Enter a File name";
+                error.ForeColor = Color.Red;
+            }
+            else if (fileNameTB.Text.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                error.Text = "Cannot use special characters";
+                error.ForeColor = Color.Red;
+            }
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]
+            {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+            };
+
+            string[] ListOfGender = new string[]
+            {
+                "Male",
+                "Female"
+            };
+
+            for (int i = 0; i < 6; i++)
+            {
+                progrmTB.Items.Add(ListOfProgram[i].ToString());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                gendTB.Items.Add(ListOfGender[i].ToString());
+            }
+        }
+    }
+}
